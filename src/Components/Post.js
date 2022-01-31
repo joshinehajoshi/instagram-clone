32a55c935b6b1d5e4a6391651e1fad5c94c626bd
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-31 18:03:20 +0530

    avatar from material ui

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,14 +3,22 @@ import '../stying/post.css';
 import Avatar from '@mui/material/Avatar';
 
 function Post() {
-  return <div>
-      <h4>UserName</h4>
-      <img 
-      className="post_image"
-      src="https://avatars.githubusercontent.com/u/40416505?v=4"
-      alt="avatar" 
+  return <div className="post">
+    <div className="post_header">
+      <Avatar 
+      className="post_avatar"
+      alt="Neha Joshi" 
+      src="/static/images/avatar/1.jpg" 
       />
-      <h4 className="post_text"><strong>UserName</strong>: Caption</h4>
+        <h4>UserName</h4>
+    </div>
+        <img 
+        className="post_image"
+        src="https://avatars.githubusercontent.com/u/40416505?v=4"
+        alt="avatar" 
+        />
+        <h4 className="post_text"><strong>UserName</strong>: Caption</h4>
+     
   </div>;
 }
 
