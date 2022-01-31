2a105585b745131f318896f0af1cffe879d20de8
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-31 15:01:01 +0530

    header logo added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app_header">
+          <img
+           className="app_headerImage"
+           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram_logo" />
+      </div>
       <h1>Hello</h1>
     </div>
   );
